24fd6d064a8b68c2f2377175f9dbd29fc0fa4ca7
Allen Fencer <allenfencer316@gmail.com>
2021-12-31 18:50:09 +0400

    added login and otp screens

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,14 +1,13 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movii/global_widgets/custom_button.dart';
 import 'package:movii/global_widgets/custom_title_text.dart';
 import 'package:movii/global_widgets/small_text.dart';
-import 'package:movii/screens/login_screen.dart';
+import 'package:movii/main.dart';
+import 'package:movii/screens/otp_screen.dart';
 
-class SignUp extends StatelessWidget {
-  const SignUp({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,39 +25,8 @@ class SignUp extends StatelessWidget {
                 ),
                 Center(
                   child: TitleText(
-                    title1: 'Sign Up',
+                    title1: 'Login',
                     title2: '',
-                  ),
-                ),
-                SmallText(
-                  text: 'Name',
-                  fontSize: 18,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 15),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(15)),
-                  clipBehavior: Clip.antiAlias,
-                  child: TextField(
-                    keyboardType: TextInputType.name,
-                    style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        color: Colors.grey[300],
-                        fontWeight: FontWeight.w400,
-                        letterSpacing: 0.8),
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 12, vertical: 18),
-                      fillColor: Color(0xff848484).withOpacity(0.7),
-                      filled: true,
-                      border: InputBorder.none,
-                      hintStyle: GoogleFonts.poppins(
-                          fontSize: 14,
-                          color: Colors.grey[300],
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: 0.8),
-                      hintText: 'Enter Your Name',
-                    ),
                   ),
                 ),
                 SmallText(
@@ -78,28 +46,29 @@ class SignUp extends StatelessWidget {
                         fontWeight: FontWeight.w400,
                         letterSpacing: 0.8),
                     decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 12, vertical: 18),
-                        fillColor: Color(0xff848484).withOpacity(0.7),
-                        filled: true,
-                        border: InputBorder.none,
-                        prefixStyle: GoogleFonts.poppins(
-                            fontSize: 16,
-                            color: Colors.grey[300],
-                            fontWeight: FontWeight.w400,
-                            letterSpacing: 0.8),
-                        prefixText: '+91 '),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 12, vertical: 18),
+                      fillColor: Color(0xff848484).withOpacity(0.7),
+                      filled: true,
+                      border: InputBorder.none,
+                      prefixText: '+91 ',
+                      prefixStyle: GoogleFonts.poppins(
+                          fontSize: 16,
+                          color: Colors.grey[300],
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.8),
+                    ),
                   ),
                 ),
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.25,
+                  height: MediaQuery.of(context).size.height * 0.4,
                 ),
                 Center(
                   child: CustomButton(
                     btnText: 'Go',
                     onTap: () {
                       Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => LoginPage()));
+                          MaterialPageRoute(builder: (context) => OTPScreen()));
                     },
                   ),
                 ),
--- lib/screens/otp_screen.dart
+++ lib/screens/otp_screen.dart
@@ -1,14 +1,13 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movii/global_widgets/custom_button.dart';
 import 'package:movii/global_widgets/custom_title_text.dart';
 import 'package:movii/global_widgets/small_text.dart';
-import 'package:movii/screens/login_screen.dart';
 
-class SignUp extends StatelessWidget {
-  const SignUp({Key? key}) : super(key: key);
+import '../main.dart';
+
+class OTPScreen extends StatelessWidget {
+  const OTPScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,44 +25,15 @@ class SignUp extends StatelessWidget {
                 ),
                 Center(
                   child: TitleText(
-                    title1: 'Sign Up',
+                    title1: 'Enter OTP',
                     title2: '',
                   ),
                 ),
-                SmallText(
-                  text: 'Name',
-                  fontSize: 18,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 15),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(15)),
-                  clipBehavior: Clip.antiAlias,
-                  child: TextField(
-                    keyboardType: TextInputType.name,
-                    style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        color: Colors.grey[300],
-                        fontWeight: FontWeight.w400,
-                        letterSpacing: 0.8),
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 12, vertical: 18),
-                      fillColor: Color(0xff848484).withOpacity(0.7),
-                      filled: true,
-                      border: InputBorder.none,
-                      hintStyle: GoogleFonts.poppins(
-                          fontSize: 14,
-                          color: Colors.grey[300],
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: 0.8),
-                      hintText: 'Enter Your Name',
-                    ),
+                Center(
+                  child: SmallText(
+                    text: 'Enter 6 digit OTP sent to\nyour mobile number.',
+                    fontSize: 18,
                   ),
-                ),
-                SmallText(
-                  text: 'Phone Number',
-                  fontSize: 18,
                 ),
                 Container(
                   margin: EdgeInsets.symmetric(vertical: 15),
@@ -88,18 +58,18 @@ class SignUp extends StatelessWidget {
                             color: Colors.grey[300],
                             fontWeight: FontWeight.w400,
                             letterSpacing: 0.8),
-                        prefixText: '+91 '),
+                        ),
                   ),
                 ),
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.25,
+                  height: MediaQuery.of(context).size.height * 0.4,
                 ),
                 Center(
                   child: CustomButton(
                     btnText: 'Go',
                     onTap: () {
                       Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => LoginPage()));
+                          MaterialPageRoute(builder: (context) => App()));
                     },
                   ),
                 ),
